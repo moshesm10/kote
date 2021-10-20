e33f149720565d8e84d9b5f21050faa816e76ea8
Aleksandr Moiseev <aimoiseev@artlebedev.ru>
2021-10-20 16:19:34 +0300

    Заверстаны основные блоки

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
-console.log('test');
+const appealButton = document.querySelector('.appeal__button');
+const appealAccentVideo = document.querySelector('.accent-video');
+const appealMainVideo = document.querySelector('.main-video');
+
+appealButton.addEventListener('click', () => {
+    appealButton.style.display = 'none';
+
+    appealMainVideo.classList.remove('hidden');
+    appealMainVideo.classList.add('active-video');
+
+    setTimeout(() => {
+        appealButton.style.display = '';
+
+        appealMainVideo.classList.add('hidden');
+        appealMainVideo.classList.remove('active-video');
+    }, 10000)
+});
